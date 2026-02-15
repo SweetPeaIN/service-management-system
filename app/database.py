e9a51796b4ecf084090f919888a39fc1d84b2499
Aryan Trivedi <cr7aryan7@gmail.com>
2026-02-15 22:42:43 +0530

    chore: simplifying commit history

--- app/database.py
+++ app/database.py
@@ -2,8 +2,8 @@ from sqlmodel import SQLModel, create_engine
 from app.models import User, ServiceRequest
 
 # sqlite_file_name = "data/database.db"
-# sqlite_file_name = "data/dummy_database.db"
-sqlite_file_name = "data/test_db.db"
+sqlite_file_name = "data/dummy_database.db"
+# sqlite_file_name = "data/test_db.db"
 
 sqlite_url = f"sqlite:///{sqlite_file_name}"
 # echo=False stops the console from showing raw SQL commands (cleaner UI)
